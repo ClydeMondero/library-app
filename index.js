9124e25a0b205909b2e6241251c9d4a9ccfa728a
Clyde Mondero <clydereinyelmondero0324@gmail.com>
2023-07-02 20:36:37 +0800

    Added create and delete book function and styles

--- index.js
+++ index.js
@@ -1,31 +1,66 @@
-// //* Storage for the books
-// let library = [];
+//* Storage for the books
+let library = [];
+let i = 0;
 
-// //* Book Protype Constructor
-// let i = 1;
-// function Book(id, title, author, page, read) {
-//   this.id = i;
-//   this.title = title;
-//   this.author = author;
-//   this.page = page;
-//   this.read = "Not Read";
-//   this.bookInfo = function () {
-//     return `${title} by ${author}, ${page} pages ${read}`;
-//   };
-// }
+//* Book Prototype Constructor
+function Book(id, title, author, pages, read) {
+  this.id = i;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.bookInfo = () => {
+    console.log(`${title} by ${author}, ${pages} pages ${read}`);
+  };
+}
 
-// //* Adds a book to the library array
-// function addBook(title, author, pages, read) {
-//   let book = new Book(title, author, pages, read);
+//* Adds a book to the library array
+function addBook(title, author, pages, read) {
+  let book = new Book(i, title, author, pages, read);
 
-//   library.push(book);
-//   i++;
-// }
+  library.push(book);
+  i++;
+}
 
-// //*Displays every book in the library array
-// library.forEach((book) => {
-//   console.log(book.bookInfo());
-// });
+//* Displays every book in the library array
+function displayBooks() {
+  booksContainer.replaceChildren();
+  library.forEach((book) => {
+    book.bookInfo();
+
+    const bookContainer = document.createElement("div");
+    bookContainer.classList.add("book");
+    const title = document.createElement("p");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const read = document.createElement("button");
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("remove");
+
+    bookContainer.appendChild(title).textContent = `Title: ${book.title}`;
+    bookContainer.appendChild(author).textContent = `Author: ${book.author}`;
+    bookContainer.appendChild(
+      pages
+    ).textContent = `Number of pages: ${book.pages}`;
+
+    if (book.read) {
+      bookContainer.appendChild(read).textContent = "Read";
+      read.classList.add("read");
+    } else {
+      bookContainer.appendChild(read).textContent = "Not Read";
+      read.classList.add("not-read");
+    }
+
+    bookContainer.appendChild(removeBtn).textContent = "Remove";
+
+    booksContainer.appendChild(bookContainer);
+
+    removeBtn.addEventListener("click", () => {
+      library.splice(book.id, 1);
+      displayBooks();
+    });
+  });
+}
 
 //* Resets add book form
 function resetForm(input) {
@@ -34,30 +69,26 @@ function resetForm(input) {
 }
 
 //* Shows the library
-function showLibrary() {
-  formContainer.style.display = "none";
-  addBookBtn.style.display = "block";
-  booksContainer.style.display = "block";
-}
+function showLibrary() {}
 
 //*Hides add book form
-function hideForm(e) {
-  if (e.target.matches(".form-container")) {
-    form.classList.replace("open", "close");
-    form.addEventListener("animationend", showLibrary, { once: true });
+function hideForm() {
+  form.classList.replace("open", "close");
+  form.addEventListener(
+    "animationend",
+    () => (formContainer.style.display = "none"),
+    { once: true }
+  );
 
-    formContainer.querySelectorAll(".input").forEach((input) => {
-      resetForm(input);
-    });
-  }
+  formContainer.querySelectorAll(".input").forEach((input) => {
+    resetForm(input);
+  });
 }
 
 //* Shows add book form
 function showForm() {
   formContainer.style.display = "flex";
   form.classList.replace("close", "open");
-  addBookBtn.style.display = "none";
-  booksContainer.style.display = "none";
 }
 
 const addBookBtn = document.querySelector(".add-book-btn");
@@ -66,13 +97,33 @@ const booksContainer = document.querySelector(".books-container");
 const formContainer = document.querySelector(".form-container");
 const form = document.querySelector(".form");
 const checkBox = document.querySelector(".checkbox");
+const addBtn = document.querySelector(".button");
+
+addBook("Percy Jackson", "Rick Riordian", 250, false);
+
+displayBooks();
 
 addBookBtn.addEventListener("click", () => {
   showForm();
+
+  form.addEventListener("submit", (e) => e.preventDefault());
+
+  addBtn.addEventListener("click", () => {
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const read = document.querySelector("#readStatus").checked;
+
+    addBook(title, author, pages, read);
+    hideForm();
+    displayBooks();
+  });
 });
 
 formContainer.addEventListener("click", (e) => {
-  hideForm(e);
+  if (e.target.matches(".form-container")) {
+    hideForm();
+  }
 });
 
 //TODO: Form Validation (required, duplicate, invalid inputs)
